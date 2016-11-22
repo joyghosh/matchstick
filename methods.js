6327bad28fd7b3158b7bf85deb3ee99cb4ddc232
Joy Ghosh <joyghosh@Joys-MacBook-Pro.local>
2016-11-22 22:32:47 +0530

    initial commit

---
+++ methods.js
@@ -0,0 +1,29 @@
+/**
+* @author: Joy Ghosh
+* @date: November 22, 2016
+**/
+function search(data, pattern){
+	var text = data.split("");
+	var pat = pattern.split("");
+
+
+}
+
+/**
+* Naive or simple approach for pattern searching.
+**/
+function naive(data, pattern){
+
+}
+
+/**
+* Naive or simple approach for pattern searching.
+**/
+function kmp(data, pattern){	
+}
+
+/**
+* Naive or simple approach for pattern searching.
+**/
+function rabinkarp(data, pattern){	
+}
