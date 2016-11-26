0eb3d4ba89b038ce9485b95f26f0cd219928e3ff
Joy Ghosh <joyghosh@Joys-MacBook-Pro.local>
2016-11-26 19:22:49 +0530

    naive pattern search

--- methods.js
+++ methods.js
@@ -6,14 +6,40 @@ function search(data, pattern){
 	var text = data.split("");
 	var pat = pattern.split("");
 
+	return naive(text, pattern);
+}
 
+/**
+* return index of first match.
+**/
+function searchFirst(data, pattern){
+	var text = data.split("");
+	var patt = pattern.split("");
+
+	return naive(text, patt)[0];
 }
 
 /**
 * Naive or simple approach for pattern searching.
 **/
 function naive(data, pattern){
+		int M = data.length;
+		int N = pattern.length;
+		var indices = [];
 
+		/* slide pattern one by one*/
+		for (var i = 0; i <= M-N; i++) {
+			var j;
+			for (j = 0; j < N; j++) {
+				if(data[i+j]!=pattern[j])
+					break;
+			}
+
+			if(j==N){
+				indices.push(i);
+			}
+		}
+		return indices;
 }
 
 /**
