2b4511146f49b1efc10574757c17045581733f3b
Joy Ghosh <joyghosh@Joys-MacBook-Pro.local>
2016-12-31 18:41:20 +0530

    kmp needs fix.

--- methods.js
+++ methods.js
@@ -2,10 +2,39 @@
 * @author: Joy Ghosh
 * @date: November 22, 2016
 **/
-exports.search = function(data, pattern){
-	var text = data.split("");
+
+//supported pattern searching methods.
+var Algorithm = { NAIVE : "naive",
+	KMP : "kmp",
+	RABIN_KARP : "rabinkarp"
+};
+
+// exports.Algorithm = algorithms;
+
+//search the entire text for matching patterns.
+exports.search = function(data, pattern, algorithm){
+	var text = data.split(""); 
 	var pat = pattern.split("");
-	var result = naive(text, pattern);
+	var M = data.length;
+	var N = pattern.length;
+	var result;
+	
+	switch(algorithm){
+		case Algorithm.NAIVE:
+			result = naive(text, pattern, M, N);
+			break;
+
+		case Algorithm.KMP:
+			result = kmp(text, pattern, M, N);
+			break;
+
+		case Algorithm.RABIN_KARP:
+			result = kmp(text, pattern, M, N);
+			break;
+
+		default:
+			console.log("No such pattern search algorithm found.");			
+	}
 	return (result.length==0)? -1: result;
 }
 
@@ -22,11 +51,8 @@ exports.searchFirst = function(data, pattern){
 /**
 * Naive or simple approach for pattern searching.
 **/
-function naive(data, pattern){
-	var M = data.length;
-	var N = pattern.length;
+function naive(data, pattern, M, N){
 	var indices = [];
-
 	/* slide pattern one by one*/
 	for (var i = 0; i <= M-N; i++) {
 		var j;
@@ -35,9 +61,7 @@ function naive(data, pattern){
 				break;
 		}
 
-		if(j==N){
-			indices.push(i);
-		}
+		if(j==N){ indices.push(i); }
 	}
 	return indices;
 }
@@ -45,11 +69,62 @@ function naive(data, pattern){
 /**
 * Naive or simple approach for pattern searching.
 **/
-function kmp(data, pattern){	
+function kmp(data, pattern, M, N){
+	var indices = [];
+	var lps = [];
+	preprocess(pattern, M, lps);
+	console.log(lps);
+	var i = 0;   // index for data.
+    var j  = 0;  // index for pattern.
+    while(i < N){
+        if (pattern[j] == data[i]){
+            ++j; ++i;
+        }
+ 
+        if(j == M){
+        	indices.push(i-j);
+            j = lps[j-1];
+        }else if (i < N && pattern[j] != data[i]){	// mismatch after j matches
+            // Do not match lps[0..lps[j-1]] characters,
+            // they will match anyway
+            if (j!=0){
+            	 j = lps[j-1];
+            }else{
+                i = i+1;
+            }
+        }
+    }
+    return indices;
 }
+
+/**
+* Preprocessing for KMP approach.
+**/
+function preprocess(pattern, M, lps){
+	var length = 0;
+	lps[0] = 0;
+
+	var i = 1;
+	while(i<M){
+		if(pattern[i] == pattern[length]){
+			++length;
+			lps[i] = length;
+			++i;
+		}else{
+			if(length!=0){
+				length = lps[length-1]; 
+			}else{
+				lps[i] = 0;
+				++i;
+			}
+		}
+	}
+}
+
 
 /**
 * Naive or simple approach for pattern searching.
 **/
-function rabinkarp(data, pattern){	
+function rabinkarp(data, pattern, M, N){
+	return [];	
 }
--- test/test.js
+++ test/test.js
@@ -8,23 +8,35 @@ var matcher = require("../methods");
 describe("pattern matcher", function(){
 	var text = ["This is a test text.", "AABAACAADAABAABA"];
 	var pattern = ["test", "AABA"];
+	// var algorithms = matcher.Algorithms;
+	// console.log(algorithms);
 
 	describe("naive pattern matching", function(){
 		it('should return [10] upon successful match.', function(){
-			assert.deepEqual([10], matcher.search(text[0], pattern[0]));
+			assert.deepEqual([10], matcher.search(text[0], pattern[0], "naive"));
 		});
 
 		it('should return [0,9,12] upon successful match.', function(){
-			assert.deepEqual([0,9,12], matcher.search(text[1], pattern[1]));
+			assert.deepEqual([0,9,12], matcher.search(text[1], pattern[1], "naive"));
 		});
 
 		it('should return -1 upon no match.', function(){
-			assert.equal(-1, matcher.search(text[0], pattern[1]));
+			assert.equal(-1, matcher.search(text[0], pattern[1], "naive"));
 		}); 
 	});
 
 	describe("KMP pattern matching", function(){
+		it('should return [10] upon successful match.', function(){
+			assert.deepEqual([10], matcher.search(text[0], pattern[0], "kmp"));
+		});
 
+		it('should return [0,9,12] upon successful match.', function(){
+			assert.deepEqual([0,9,12], matcher.search(text[1], pattern[1], "kmp"));
+		});
+
+		it('should return -1 upon no match.', function(){
+			assert.equal(-1, matcher.search(text[0], pattern[1], "kmp"));
+		});
 	});
 
 	describe("Rabin Karp pattern matching", function(){
